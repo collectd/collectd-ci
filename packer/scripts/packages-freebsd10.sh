f2d495cdf7777c517c31e42c49f332aea9dd2e48
Marc Fournier <marc.fournier@camptocamp.com>
2015-04-14 23:14:41 +0200

    packer template for freebsd10 build slave

---
+++ packer/scripts/packages-freebsd10.sh
@@ -0,0 +1,4 @@
+
+sudo pkg install -y autoconf automake bison gdb git libtool m4 pkgconf valgrind
+
+sudo pkg install -y openjdk8 gnutls libdbi libesmtp libgcrypt glib hiredis openldap-sasl-client libltdl libmemcached libmodbus mysql56-client libnotify openipmi liboping libpcap postgresql94-client protobuf protobuf-c rabbitmq-c-devel rrdtool net-snmp libstatgrab tokyocabinet tokyotyrant nut varnish4 libvirt libxml2 yajl python34 libsigrok mongo-c-driver-devel librouteros
