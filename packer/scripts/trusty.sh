d328cbb9e3b9447bcec540c3bb593ffe82d7173e
Marc Fournier <marc.fournier@camptocamp.com>
2015-04-18 01:13:52 +0200

    install additional compiler versions into build environments

--- packer/scripts/trusty.sh
+++ packer/scripts/trusty.sh
@@ -6,7 +6,7 @@ echo 'APT::Install-Suggests "0";' > /etc/apt/apt.conf.d/50no-install-suggests
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison cpp flex gcc gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison clang clang-3.3 clang-3.5 cpp flex gcc gcc-4.4 gcc-4.6 gcc-4.7 gcc-4.8 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhiredis-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmnl-dev libmodbus-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libow-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librabbitmq-dev librdkafka-dev librrd-dev libsensors4-dev libstatgrab-dev libsigrok-dev libsnmp-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean
