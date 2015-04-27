collectd/collectd-ci

commit d5e84eb3d753ddcbb8aa27c01effef568737985e

install dpkg-dev, which provides dpkg-buildflags

packer/scripts/wheezy.sh
@@ -10,7 +10,7 @@ sysctl -p
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison clang cpp flex gcc gcc-4.4 gcc-4.6 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison clang cpp dpkg-dev lex gcc gcc-4.4 gcc-4.6 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iproute-dev iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhal-dev libhiredis-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmnl-dev libmodbus-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libow-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librabbitmq-dev librrd-dev libsensors4-dev libsnmp-dev libstatgrab-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient1-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean

packer/scripts/precise.sh
@@ -9,7 +9,7 @@ sysctl -p
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison clang cpp flex gcc gcc-4.4 gcc-4.5 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison clang cpp dpkg-dev flex gcc gcc-4.4 gcc-4.5 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iproute-dev iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhal-dev libhiredis-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmnl-dev libmodbus-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libow-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librabbitmq-dev librrd-dev libsensors4-dev libsnmp-dev libstatgrab-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient1-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean

packer/scripts/squeeze.sh
@@ -10,7 +10,7 @@ sysctl -p
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison cpp flex gcc gcc-4.1 gcc-4.3 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison cpp dpkg-dev flex gcc gcc-4.1 gcc-4.3 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iproute-dev iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhal-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librrd-dev libsensors4-dev libsnmp-dev libstatgrab-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient1-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean

packer/scripts/trusty.sh
@@ -9,7 +9,7 @@ sysctl -p
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison clang cpp flex gcc gcc-4.4 gcc-4.6 gcc-4.7 gcc-4.8 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison clang cpp dpkg-dev flex gcc gcc-4.4 gcc-4.6 gcc-4.7 gcc-4.8 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhiredis-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmnl-dev libmodbus-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libow-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librabbitmq-dev librdkafka-dev librrd-dev libsensors4-dev libstatgrab-dev libsigrok-dev libsnmp-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean

commit b6b6f13d97766eab4b13fb388020e4735ec920df

fix typo introduced in d5e84eb3

packer/scripts/wheezy.sh
@@ -10,7 +10,7 @@ sysctl -p
 apt-get -y update
 apt-get -y upgrade
 
-apt-get -y install autoconf automake bison clang cpp dpkg-dev lex gcc gcc-4.4 gcc-4.6 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
+apt-get -y install autoconf automake bison clang cpp dpkg-dev flex gcc gcc-4.4 gcc-4.6 gdb git libc6-dev libtool m4 make pkg-config strace valgrind
 apt-get -y install autotools-dev default-jdk iproute-dev iptables-dev javahelper libatasmart-dev libcurl4-gnutls-dev libdbi0-dev libesmtp-dev libganglia1-dev libgcrypt11-dev libglib2.0-dev libhal-dev libhiredis-dev libi2c-dev libldap2-dev libltdl-dev liblvm2-dev libmemcached-dev libmnl-dev libmodbus-dev libmysqlclient-dev libnotify-dev libopenipmi-dev liboping-dev libow-dev libpcap-dev libperl-dev libpq-dev libprotobuf-c0-dev librabbitmq-dev librrd-dev libsensors4-dev libsnmp-dev libstatgrab-dev libtokyocabinet-dev libtokyotyrant-dev libudev-dev libupsclient1-dev libvarnish-dev libvirt-dev libxml2-dev libyajl-dev linux-libc-dev perl protobuf-c-compiler python-dev
 
 apt-get -y clean