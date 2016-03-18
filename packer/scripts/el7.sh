288d914a5397cf5bc3ff7ae15510b357f4587a25
Marc Fournier <marc.fournier@camptocamp.com>
2016-03-18 10:26:49 +0100

    Build modbus plugin on EL7

--- packer/scripts/el7.sh
+++ packer/scripts/el7.sh
@@ -8,7 +8,7 @@ yum -y update
 
 yum -y install autoconf automake bison clang cpp curl flex gcc gdb git glibc-devel libgcrypt-devel libtool libtool-ltdl-devel m4 make nc pkgconfig redhat-rpm-config strace tar valgrind
 
-yum -y install OpenIPMI-devel ganglia-devel gtk2-devel gpsd-devel iproute-devel iptables-devel java-1.7.0-openjdk-devel java-devel jpackage-utils libatasmart-devel libcap-devel libcurl-devel libdbi-devel libesmtp-devel hiredis-devel libmemcached-devel libmnl-devel mosquitto-devel libnotify-devel openldap-devel liboping-devel libpcap-devel librabbitmq-devel libudev-devel libvirt-devel libxml2-devel lm_sensors-devel lvm2-devel mysql-devel net-snmp-devel nut-devel owfs-devel openldap-devel perl-ExtUtils-Embed postgresql-devel protobuf-c-devel python-devel rrdtool-devel varnish-libs-devel xmms-devel yajl-devel
+yum -y install OpenIPMI-devel ganglia-devel gtk2-devel gpsd-devel iproute-devel iptables-devel java-1.7.0-openjdk-devel java-devel jpackage-utils libatasmart-devel libcap-devel libcurl-devel libdbi-devel libesmtp-devel hiredis-devel libmemcached-devel libmnl-devel mosquitto-devel libnotify-devel openldap-devel libmodbus-devel liboping-devel libpcap-devel librabbitmq-devel libudev-devel libvirt-devel libxml2-devel lm_sensors-devel lvm2-devel mysql-devel net-snmp-devel nut-devel owfs-devel openldap-devel perl-ExtUtils-Embed postgresql-devel protobuf-c-devel python-devel rrdtool-devel varnish-libs-devel xmms-devel yajl-devel
 
 yum -y clean all
 
