597a68e9596002b01894472a9e7be9f2229169ca
Ruben Kerkhof <ruben@rubenkerkhof.com>
2016-03-26 19:57:10 +0100

    Install the xfsprogs headers
    utils_mount.c can optionally use XFS.
    To detect breakage like
    https://github.com/collectd/collectd/commit/865a6c83250e3d4381596a0d937df31d563f97c6,
    install the xfsprogs headers.

--- packer/scripts/el6.sh
+++ packer/scripts/el6.sh
@@ -13,7 +13,7 @@ yum -y update
 
 yum -y install autoconf automake bison clang cpp curl flex gcc gdb git glibc-devel libgcrypt-devel libtool libtool-ltdl-devel m4 make nc pkgconfig redhat-rpm-config strace tar valgrind
 
-yum -y install OpenIPMI-devel ganglia-devel iproute-devel iptables-devel java-1.7.0-openjdk-devel java-devel jpackage-utils libatasmart-devel libcap-devel libcurl-devel libdbi-devel libesmtp-devel libmemcached-devel libmnl-devel libmodbus-devel libnotify-devel liboping-devel libpcap-devel librabbitmq-devel libstatgrab-devel libvirt-devel libxml2-devel lm_sensors-devel lvm2-devel mysql-devel net-snmp-devel nut-devel openldap-devel perl-ExtUtils-Embed postgresql-devel protobuf-c-devel python-devel rrdtool-devel varnish-libs-devel yajl-devel hiredis-devel libudev-devel
+yum -y install OpenIPMI-devel ganglia-devel iproute-devel iptables-devel java-1.7.0-openjdk-devel java-devel jpackage-utils libatasmart-devel libcap-devel libcurl-devel libdbi-devel libesmtp-devel libmemcached-devel libmnl-devel libmodbus-devel libnotify-devel liboping-devel libpcap-devel librabbitmq-devel libstatgrab-devel libvirt-devel libxml2-devel lm_sensors-devel lvm2-devel mysql-devel net-snmp-devel nut-devel openldap-devel perl-ExtUtils-Embed postgresql-devel protobuf-c-devel python-devel rrdtool-devel varnish-libs-devel yajl-devel hiredis-devel libudev-devel xfsprogs-devel
 
 yum -y clean all
 
